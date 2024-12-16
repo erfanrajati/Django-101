f9bab868aa7bde602e0756480a65bd28c58be546
Erfan Rajati Haghi <main.e.rajati@gmail.com>
2024-12-16 19:49:55 +0330

    404 error handling

--- 9_MasterTemp/MyProject/app1/urls.py
+++ 9_MasterTemp/MyProject/app1/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("app1", views.index, name="app1")
+    path("app1", views.index, name="app1"),
+    path("<dynamic>", views.not_found, name="not_found")
 ]
--- 9_MasterTemp/MyProject/app1/views.py
+++ 9_MasterTemp/MyProject/app1/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from django.template.loader import render_to_string
+from django.http import HttpResponseNotFound
 # Create your views here.
 def index(request):
     return render(request, "app1/index.html")
+
+
+def not_found(request, dynamic):
+    response = render_to_string('404.html')
+    return HttpResponseNotFound(response)
