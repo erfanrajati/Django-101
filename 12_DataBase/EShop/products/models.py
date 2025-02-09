2c53b7af32d7328228952071a83161bfa9cd32eb
Erfan Rajati Haghi <main.e.rajati@gmail.com>
2025-02-10 00:28:13 +0330

    Update session 12

--- 12_DataBase/EShop/products/models.py
+++ 12_DataBase/EShop/products/models.py
@@ -1,12 +1,18 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Product(models.Model):
     # Attributes
-    title = models.CharField(max_length=150)
-    price = models.IntegerField()
+    title = models.CharField(default=None, max_length=150)
+    price = models.IntegerField(default=None)
+    rating = models.IntegerField(default=None, validators=[MinValueValidator(0), MaxValueValidator(5)], null=True)
+    short_description = models.CharField(default=None, max_length=350, null=True)
+    is_active = models.BooleanField(default=True, null=True) # By default, this field is not Nullable, so the default value can't be None either.
 
     # Overriding __str__ method
     def __str__(self):
         return f"{self.title} {self.price}"
+    
+
