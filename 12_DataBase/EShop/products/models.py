29f96cb7b2ff82bcc154ffeffaabb44261ca4b28
Erfan Rajati Haghi <main.e.rajati@gmail.com>
2025-02-09 20:13:42 +0330

    Update session 12

--- 12_DataBase/EShop/products/models.py
+++ 12_DataBase/EShop/products/models.py
@@ -6,3 +6,7 @@ class Product(models.Model):
     # Attributes
     title = models.CharField(max_length=150)
     price = models.IntegerField()
+
+    # Overriding __str__ method
+    def __str__(self):
+        return f"{self.title} {self.price}"
